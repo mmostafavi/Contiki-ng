b59b249286b58ebab159a56e6e75b3de3c4dd97f
Mahdi Mostafavi <mmostafavi247@gamil.com>
2021-05-14 03:32:53 +0430

    Add demo-sensor

---
+++ demo-sensor/mysensor.h
@@ -0,0 +1,11 @@
+// mysensor.h
+#ifndef MYSENSOR_H
+#define MYSENSOR_H
+struct Sensor {
+  char name[15];
+  float value;
+};
+
+struct Sensor read_temperature();
+struct Sensor read_humidity();
+#endif
